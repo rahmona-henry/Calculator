7c8c9cffd485802938cd75b3b8e1ea88a6d2df0f
rahmona-henry <someone@devacademy.co.nz>
2016-02-07 12:50:54 +1300

    Add js

--- main.js
+++ main.js
@@ -3,66 +3,59 @@ $(document).ready(function () {
     if (number.length > 9) {
       totaldiv.text(number.substr(number.length - 9, 9));
       if (number.length > 15) {
-        number = " ";
-        totaldiv.text("Error");
-            }
-        } 
-    };
+        number = '';
+        totaldiv.text('Error');
+      }
+    } 
+  };
 
-    var number = " ";
-    var newnumber = " ";
-    var operator = " ";
-    var totaldiv = $("#total");
-    totaldiv.text("0");
+  var number = ' ';
+  var newnumber = ' ';
+  var operator = ' ';
+  var totaldiv = $('#total');
+  totaldiv.text('0');
 
-    $("#numbers > a").not("#clear,#clearall").click(function(){
-        number += $(this).text();
-        totaldiv.text(number);
-        testNumLength(number);
+    $('#numbers > a').not('#clear,#clearall').click(function () {
+      number += $(this).text();
+      totaldiv.text(number);
+      testNumLength(number);
     });
-    $("#operators > a").not("#equals").click(function(){
-        operator = $(this).text();
-        newnumber = number;
-        number = "";
-        totaldiv.text("0");
-    });
+  $('#operators > a').not('#equals').click(function(){
+    operator = $(this).text();
+    newnumber = number;
+    number = '';
+    totaldiv.text('0');
+  });
 
-      $("#clear").click(function() {
-     number = "";
-      totaldiv.text("0");
+      $('#clear').click(function () {
+        number = '';
+        totaldiv.text('0');
       });
 
-     $("#clearall").click(function() {
-     number = "";
-     totaldiv.text("0");
-     newnumber = "";
+     $('#clearall').click(function() {
+       number = '';
+       totaldiv.text('0');
+       newnumber = '';
      });
-     
-   $("#equals").click(function(){
-    number = parseInt(number,10);
-    newnumber = parseInt(newnumber,10);
+  $('#equals').click(function () {
+    number = parseInt(number, 10);
+    newnumber = parseInt(newnumber, 10);
     var result;
-    if (operator==="+"){
-        result = newnumber+number;
-    }
-    else if (operator==="-"){
-        result = newnumber - number;
-    }
-    else if (operator==="*"){
-        result = newnumber * number;
-    }
-    else if (operator==="/"){
-        result = newnumber / number;
+    if (operator === '+') {
+      result = newnumber + number;
+    } else if (operator === '-') {
+      result = newnumber - number;
+    } else if (operator === '*') {
+      result = newnumber * number;
+    } else if (operator === '/') {
+      result = newnumber / number;
     }
 
     result = result.toString(10);
     totaldiv.text(result);
     testNumLength(result);
-    number = "";
-    newnumber = "";
+    number = '';
+    newnumber = '';
+});   
 });
-   
-});
-
-
 
