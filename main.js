006be54ccc5ef1f31fa9cd4e628a185fdfb50e95
rahmona-henry <someone@devacademy.co.nz>
2016-02-07 11:59:32 +1300

    Add js

--- main.js
+++ main.js
@@ -1,61 +1,64 @@
-$(document).ready(function () {
-  var testNumLength = function (number) {
-    if (number.length > 9) {
-      totaldiv.text(number.substr(number.length - 9, 9));
-      if (number.length > 15) {
-        number = ' ';
-        totaldiv.text('Error');
-      }
-    }
-  };
+$(document).ready(function(){
+    var testNumLength = function(number) {
+        if (number.length > 9) {
+            totaldiv.text(number.substr(number.length-9,9));
+            if (number.length > 15) {
+                number = "";
+                totaldiv.text("Err");
+            }
+        } 
+    };
+    var number = "";
+    var newnumber = "";
+    var operator = "";
+    var totaldiv = $("#total");
+    totaldiv.text("0");
+    $("#numbers > a").not("#clear,#clearall").click(function(){
+        number += $(this).text();
+        totaldiv.text(number);
+        testNumLength(number);
+    });
+    $("#operators > a").not("#equals").click(function(){
+        operator = $(this).text();
+        newnumber = number;
+        number = "";
+        totaldiv.text("0");
+    });
 
-  var number = ' ';
-  var newnumber = ' ';
-  var operator = ' ';
-  var totaldiv = $('#total');
-  totaldiv.text('0');
-
-  $('#numbers a').not('#clear,#clearall').click(function () {
-    number += this.text();
-    totaldiv.text(number);
-    testNumLength(number);
-  });
-
-  $('#operators a').not('#equals').click(function () {
-    operator = $(this).text();
-    newnumber = number;
-    number = ' ';
-    totaldiv.text('0');
-  });
-
-  $('#clear, #clearall').click(function () {
-    number = ' ';
-    totaldiv.text('0');
-    if($(this).attr('id') = 'clearall') {
-    new number = ' ';
-    }
-
-  $('#equals').click(function () {
-    number = parseInt(number, 10);
-    newnumber = parseInt(newnumber, 10);
+      $("#clear").click(function() {
+     number = "";
+      totaldiv.text("0");
+      });
+     $("#clearall").click(function() {
+     number = "";
+     totaldiv.text("0");
+     newnumber = "";
+     });
+     
+   $("#equals").click(function(){
+    number = parseInt(number,10);
+    newnumber = parseInt(newnumber,10);
     var result;
-        if (operator === '+') {
-      result = newnumber + number;
-    } else if (operator === '-'){
-      result = newnumber - number;
-
-    } else if (operator === '*') {
-      result = newnumber * number;
-
-    } else if (operator === '/') {
-      result = newnumber / number;
+    if (operator==="+"){
+        result = newnumber+number;
+    }
+    else if (operator==="-"){
+        result = newnumber - number;
+    }
+    else if (operator==="*"){
+        result = newnumber * number;
+    }
+    else if (operator==="/"){
+        result = newnumber / number;
     }
 
     result = result.toString(10);
     totaldiv.text(result);
     testNumLength(result);
-    number = ' ';
-    newnumber = ' ';
-  });
+    number = "";
+    newnumber = "";
 });
- 
+   
+});
+//.not(#equals
+
