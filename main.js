e30034421d534502da78ad8c16e71e37d69c3653
rahmona-henry <someone@devacademy.co.nz>
2016-02-07 13:09:59 +1300

    Add comments

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+
+// The testNumLength function, is used to make sure that the total
+// div doesn't overflow i.e the screen the users sees does not overflow with numbers.
   var testNumLength = function(number) {
     if (number.length > 9) {
       totaldiv.text(number.substr(number.length - 9, 9));
@@ -8,18 +11,33 @@ $(document).ready(function () {
       }
     } 
   };
-
+// Declare number, newnumber, and operator as empty strings. Declare the variable totaldiv to the
+// div with the id total. Set the .text() of totaldiv to "0.
   var number = ' ';
   var newnumber = ' ';
   var operator = ' ';
   var totaldiv = $('#total');
   totaldiv.text('0');
 
+// .click() listeners are used to receive the clicks from the numbers and operators,
+// A .click() listener is added to the #numbers a's. Noting that the #clear and #clearall
+// buttons are also in the #numbers div. not() is used to exclude the #clear and #clearall buttons),
+// Take the .text() of the button, append that to number, set the .text() of totaldiv to number, 
+// and call testNumLength, passing in number as the parameter.
+
+
     $('#numbers > a').not('#clear,#clearall').click(function () {
       number += $(this).text();
       totaldiv.text(number);
       testNumLength(number);
     });
+
+// Add a single .click() to handle all of the #operators a clicks,
+// except for the #equals button (Again, use .not() to exclude it).
+// The .click() takes the .text() of the button, and sets the
+// operator variable to that. Set newnumber to number and set number to ""
+// Set the .text() of totaldiv to "0"
+
   $('#operators > a').not('#equals').click(function(){
     operator = $(this).text();
     newnumber = number;
